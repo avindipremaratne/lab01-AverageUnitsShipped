e520ce56df599e3f43f962ec18e1581c603c6f2d
avindipremaratne <47543302+avindipremaratne@users.noreply.github.com>
2019-02-22 21:39:49 -0500

    Add files via upload

---
+++ AverageUnitsShipped/AverageUnitsShipped/Form1.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lbDays = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.lbDisplayAverage = New System.Windows.Forms.Label()
+        Me.tbInput = New System.Windows.Forms.TextBox()
+        Me.lbUnits = New System.Windows.Forms.Label()
+        Me.tbDisplay = New System.Windows.Forms.TextBox()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'lbDays
+        '
+        Me.lbDays.AutoSize = True
+        Me.lbDays.Location = New System.Drawing.Point(188, 15)
+        Me.lbDays.Name = "lbDays"
+        Me.lbDays.Size = New System.Drawing.Size(0, 13)
+        Me.lbDays.TabIndex = 2
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(188, 232)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(82, 23)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "E&xit"
+        Me.ToolTip1.SetToolTip(Me.btnExit, "Exits the program")
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(100, 232)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(83, 23)
+        Me.btnReset.TabIndex = 6
+        Me.btnReset.Text = "&Reset"
+        Me.ToolTip1.SetToolTip(Me.btnReset, "Resets the values")
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(14, 232)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(80, 23)
+        Me.btnEnter.TabIndex = 5
+        Me.btnEnter.Text = "&Enter"
+        Me.ToolTip1.SetToolTip(Me.btnEnter, "Enters the value")
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'lbDisplayAverage
+        '
+        Me.lbDisplayAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbDisplayAverage.Location = New System.Drawing.Point(44, 207)
+        Me.lbDisplayAverage.Name = "lbDisplayAverage"
+        Me.lbDisplayAverage.Size = New System.Drawing.Size(200, 23)
+        Me.lbDisplayAverage.TabIndex = 4
+        Me.lbDisplayAverage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.ToolTip1.SetToolTip(Me.lbDisplayAverage, "Display average")
+        '
+        'tbInput
+        '
+        Me.tbInput.Location = New System.Drawing.Point(95, 15)
+        Me.tbInput.Name = "tbInput"
+        Me.tbInput.Size = New System.Drawing.Size(61, 20)
+        Me.tbInput.TabIndex = 1
+        Me.ToolTip1.SetToolTip(Me.tbInput, "Inputs value")
+        '
+        'lbUnits
+        '
+        Me.lbUnits.Location = New System.Drawing.Point(47, 18)
+        Me.lbUnits.Name = "lbUnits"
+        Me.lbUnits.Size = New System.Drawing.Size(31, 13)
+        Me.lbUnits.TabIndex = 0
+        Me.lbUnits.Text = "&Units"
+        Me.lbUnits.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'tbDisplay
+        '
+        Me.tbDisplay.Location = New System.Drawing.Point(53, 53)
+        Me.tbDisplay.Multiline = True
+        Me.tbDisplay.Name = "tbDisplay"
+        Me.tbDisplay.Size = New System.Drawing.Size(173, 140)
+        Me.tbDisplay.TabIndex = 3
+        Me.ToolTip1.SetToolTip(Me.tbDisplay, "Outputs the values entered")
+        '
+        'MainForm
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(285, 276)
+        Me.Controls.Add(Me.tbDisplay)
+        Me.Controls.Add(Me.lbDays)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.lbDisplayAverage)
+        Me.Controls.Add(Me.tbInput)
+        Me.Controls.Add(Me.lbUnits)
+        Me.KeyPreview = True
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "MainForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Avearge Units Shipped"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private WithEvents lbDays As Label
+    Private WithEvents btnExit As Button
+    Private WithEvents btnReset As Button
+    Private WithEvents btnEnter As Button
+    Private WithEvents lbDisplayAverage As Label
+    Private WithEvents tbInput As TextBox
+    Private WithEvents lbUnits As Label
+    Friend WithEvents tbDisplay As TextBox
+    Friend WithEvents ToolTip1 As ToolTip
+End Class
---
+++ AverageUnitsShipped/AverageUnitsShipped/Form1.vb
@@ -0,0 +1,130 @@
+﻿'Program:       AverageUnitsShipped
+
+'Author:        Avindi Premaratne
+'Date:          January 24th 2019  
+'Description:   A program which calculates the average units shipped per week by taking the input for each day in the week
+''              and calculate and display the average to the user.
+
+
+Option Strict On
+Imports System.Text.RegularExpressions
+
+Public Class MainForm
+    Dim userInput As Integer                'stores the user input
+    Dim days(6) As Integer                  'declaring the array to store the units per week
+    Const arrayLength As Integer = 7        'stores the array length
+    Dim totalUnits As Integer               'stores the total of all the units per week
+    Dim count As Integer = 1                'stores the counter value
+    Dim minNumber As Integer = 0            'stores the minimum number of the values for validation
+    Dim maxNumber As Integer = 1000         'stores the maximum number of the values for validation
+    Dim averageUnits As Double              'stores the average of the units per week
+
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+
+    End Sub
+    Private Sub lblDays_Click_1(sender As Object, e As EventArgs) Handles btnEnter.Click
+        'Displays the current day
+        lbDays.Text = "Day " & count
+        Me.Show()
+    End Sub
+
+
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click, lbDays.Click
+        'Accept the input and check whether the value is numeric
+        If Integer.TryParse(tbInput.Text, userInput) Then
+
+            'Checks whether the input is within the range 
+            If (userInput >= minNumber) And (userInput <= maxNumber) Then
+
+                'Assign the value to the array based on the counter value
+                days(count - 1) = userInput
+
+                'Display the value in the output texbox and goes to the next line
+                tbDisplay.Text += days(count - 1) & vbCrLf
+
+                'Checks whether the counter is equal to 7 to calculate the total and avarage
+                If count = 7 Then
+
+                    'Disables Enter button and Input textbox
+                    btnEnter.Enabled = False
+                    tbInput.Enabled = False
+
+                    'Calculats the total units using For loop
+                    For count1 = 1 To arrayLength
+                        totalUnits += days(count1 - 1)
+                    Next
+
+                    'Calculate the average,converts it double and store it in averageUnits varialble
+                    averageUnits = CDbl(totalUnits / arrayLength)
+
+                    'Displays the average to two decimal places in the lblDisplayAverage label 
+                    lbDisplayAverage.Text = "Avearge per day: " + Math.Round(averageUnits, 2).ToString
+                End If
+
+                'Clears the input textbox,set forcus on it and add one value to array index
+                tbInput.Clear()
+                tbInput.Focus()
+                count = count + 1
+
+                'Displays an error message if the input is less than 0 and greater than 1000
+            Else
+                MessageBox.Show("The value must be between 0 and 1000. Please try again.")
+
+                'Clears the textbox
+                tbInput.Clear()
+
+            End If
+
+            'Displays an error message if the input is not numeric
+        Else
+            MessageBox.Show("The value must be a whole number. Please try again.")
+
+            'Clears the textbox
+            tbInput.Clear()
+        End If
+
+
+
+
+    End Sub
+
+    Private Sub MainForm_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.Escape Then
+            tbInput.Clear()
+            tbDisplay.Clear()
+            lbDisplayAverage.Text = ""
+            lbDays.Text = "Day 1"
+        End If
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Exits the program
+        Application.Exit()
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+
+        'Resets all the components when Escape key is pressed
+        If e.KeyCode = Keys.Escape Then
+            tbInput.Clear()
+            tbDisplay.Clear()
+            lbDisplayAverage.Text = ""
+            lbDays.Text = "Day 1"
+        End If
+
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        'Clears all the textboxes, enables all the buttons and other components
+        tbInput.Enabled = True
+        tbDisplay.Clear()
+        lbDisplayAverage.Text = ""
+        lbDays.Text = "Day 1"
+        btnEnter.Enabled = True
+        ReDim days(7)
+    End Sub
+End Class
